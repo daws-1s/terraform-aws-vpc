ede87c88cd82b8b928e78aa1faabf33d98c40e35
divya kamatham <divyakamatham12@gmai.com>
2024-10-01 04:44:45 +0800

    Add files

--- outputs.tf
+++ outputs.tf
@@ -2,25 +2,17 @@ output  "vpc_id" {
   value       = aws_vpc.main.id
 }
 
-# output "az_info" {
-#     value = data.aws_availability_zones.available
-# }
+output "public_subnet_ids"{
+  value = aws_subnet.public[*].id
+}
 
-# output  "vpc_id" {
-#   value       = aws_vpc.main.id
-# }
+output "private_subnet_ids"{
+  value = aws_subnet.private[*].id
+}
 
-# output "public_subnet_ids"{
-#   value = aws_subnet.public[*].id
-# }
-
-# output "private_subnet_ids"{
-#   value = aws_subnet.private[*].id
-# }
-
-# output "database_subnet_ids"{
-#   value = aws_subnet.database[*].id
-# }
+output "database_subnet_ids"{
+  value = aws_subnet.database[*].id
+}
 # output "az_info" {
 #     value = data.aws_availability_zones.available
 # }
--- variable.tf
+++ variable.tf
@@ -1,38 +1,38 @@
 variable "project_name" {
-  type = string
+    type = string
 }
 
 variable "environment" {
-  type = string
+    type = string
 }
 
 variable "vpc_cidr" {
-  #default = "10.0.0.0/16"
+    #default = "10.0.0.0/16"
 }
 
 variable "enable_dns_hostnames" {
-  default = true
+    default = true
 }
 
 # Optional
 variable "common_tags" {
-  default = {}
+    default = {}
 }
 
 variable "vpc_tags" {
-  default = {}
+    default = {}
 }
 
 variable "igw_tags" {
-  default = {}
+    default = {}
 }
 
 variable "public_subnet_cidrs" {
-  type = list
-  validation {
-    condition     = length(var.public_subnet_cidrs) == 2
-    error_message = "Please provide 2 valid public subnet CIDR"
-  }
+    type = list
+    validation {
+        condition = length(var.public_subnet_cidrs) == 2
+        error_message = "Please provide 2 valid public subnet CIDR"
+    }
 }
 
 variable "public_subnet_tags" {
@@ -40,11 +40,11 @@ variable "public_subnet_tags" {
 }
 
 variable "private_subnet_cidrs" {
-  type = list
-  validation {
-    condition     = length(var.private_subnet_cidrs) == 2
-    error_message = "Please provide 2 valid private subnet CIDR"
-  }
+    type = list
+    validation {
+        condition = length(var.private_subnet_cidrs) == 2
+        error_message = "Please provide 2 valid private subnet CIDR"
+    }
 }
 
 variable "private_subnet_tags" {
@@ -52,11 +52,11 @@ variable "private_subnet_tags" {
 }
 
 variable "database_subnet_cidrs" {
-  type = list
-  validation {
-    condition     = length(var.database_subnet_cidrs) == 2
-    error_message = "Please provide 2 valid database subnet CIDR"
-  }
+    type = list
+    validation {
+        condition = length(var.database_subnet_cidrs) == 2
+        error_message = "Please provide 2 valid database subnet CIDR"
+    }
 }
 
 variable "database_subnet_tags" {
